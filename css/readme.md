800784e4e88eeb52063730db51be9aebc8afdabe
DemetrioPadre <demetriopadre@gmail.com>
2024-01-10 16:49:56 +0100

    readme aggiornato

--- css/readme.md
+++ css/readme.md
@@ -3,3 +3,4 @@ Chiedere l età al passeggero
 Il prezzo del biglietto è di 0.21€ a km
 I < 18 hanno uno sconto del 20%
 I > di 65 hanno uno sconto del 40%
+l esercizio risulta uguale a quell assegnato senza html e css, possiamo usare la stessa base ma poi bisogna modificarne gli if trasformandoli in funzioni function trovandoli negli elementi dell html
---
+++ js/main.js
@@ -0,0 +1,29 @@
+//recupero la costante di risultato
+const resultElement = document.getElementById('main-title');
+
+
+//chiedere all utente i km che deve percorrere
+const kmNeeded = prompt('Quanti km devi percorrere?');
+
+//chiedere all utente quanti anni ha
+const age = prompt('Quanti anni hai?');
+
+//prezzo parziale del biglietto
+const partialTicket = kmNeeded * 0.21;
+
+
+
+//tipologia di sconto
+let sconto20 = partialTicket * 0.2;
+let sconto40 = partialTicket * 0.4;
+
+//variabile del biglietto che parte da 0
+let ticket = 0;
+
+//calcolatore di sconto in base all età dell utente
+if (age < 18) {
+    ticket = partialTicket - sconto20;
+
+} else if (age >= 65) {
+    ticket = partialTicket - sconto40;
+}
