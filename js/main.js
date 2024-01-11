7f4fb0da38c9a9174010dac4e45621206ed50d4c
DemetrioPadre <demetriopadre@gmail.com>
2024-01-11 02:00:09 +0100

    add remove and none

--- js/main.js
+++ js/main.js
@@ -28,6 +28,10 @@ let ticket = 0;
 
 
 printButton.addEventListener('click', function () {
+    document.getElementById('opacity').classList.add('d-none');
+    document.getElementById('barProgress').classList.add('d-none');
+    document.getElementById('barComplete').classList.remove('d-none');
+
     const userAgeselection = userAgeInput.value;
     // quanti km devi fare
     const userKm = parseInt(userKmInput.value);
@@ -43,26 +47,31 @@ printButton.addEventListener('click', function () {
     if (userAgeselection == 'minori') {
         ticket = partialTicket - sconto20;
 
-
-
     } else if (userAgeselection == 'senior') {
         ticket = partialTicket - sconto40;
 
     } else {
         ticket = partialTicket;
-
-
     }
     userGenerate.innerText = userNameInput.value;
 
     //stampiamo in html
-    finalTicket.innerText = 'Il tuo biglietto è ' + ticket.toFixed(2) + '€' + 'ricordati di obbliterarlo prima di salire sul treno';
+    // finalTicket.innerText = 'Il tuo biglietto è ' + ticket.toFixed(2) + '€' + 'ricordati di obbliterarlo prima di salire sul treno';
+    document.getElementById('finalTicket').innerText = ticket.toFixed(2) + '€';
 
 
     //carrozze random
     document.getElementById('carrozza').innerText = Math.floor(Math.random() * 10 + 1);
     //codice random sedile
     document.getElementById('codiceCp').innerText = Math.floor(Math.random() * 999 + 1);
+
+    //add and remove text over the ticket
+    document.getElementById('prepair').classList.add('d-none');
+
+    //add ticket that was d-none
+    document.getElementById('ticketContainer').classList.remove('d-none');
+
+
 })
 
 
